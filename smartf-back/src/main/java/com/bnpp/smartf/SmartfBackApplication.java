67eb108f40ba529ce4dc67c7a995715d1c41df89
Mohamed Kalboussi <mohamedkalboussi@live.fr>
2018-02-01 20:07:51 +0100

    add Back project

---
+++ smartf-back/src/main/java/com/bnpp/smartf/SmartfBackApplication.java
@@ -0,0 +1,12 @@
+package com.bnpp.smartf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfBackApplication.class, args);
+	}
+}
